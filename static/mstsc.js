6aa8b9fcf72aaba28a95c6eace6658c861531671
G07cha <azizovkostya97@gmail.com>
2015-06-26 18:16:22 +0200

    pointer input

--- static/mstsc.js
+++ static/mstsc.js
@@ -18,28 +18,81 @@
  */
 
 (function() {
+	function mouseButtonMap(button) {
+		switch(button) {
+		case 0:
+			return 1;
+		case 2:
+			return 3;
+		default:
+			return 0;
+		}
+	};
+	
 	function Mstsc(canvas) {
 		this.canvas = canvas;
 		this.ctx = canvas.getContext("2d");
+		this.socket = null;
+		
+		var self = this;
+		
+		// bind mouse move event
+		this.canvas.addEventListener('mousemove', function (e) {
+			if (!self.socket) {
+				return;
+			}
+			self.socket.emit('mouse', e.clientX, e.clientY, 0, false);
+		});
+		
+		this.canvas.addEventListener('mousedown', function (e) {
+			if (!self.socket) {
+				return;
+			}
+			self.socket.emit('mouse', e.clientX, e.clientY, mouseButtonMap(e.button), true);
+			e.preventDefault();
+		});
+		
+		this.canvas.addEventListener('mouseup', function (e) {
+			if (!self.socket) {
+				return;
+			}
+			self.socket.emit('mouse', e.clientX, e.clientY, mouseButtonMap(e.button), false);
+			e.preventDefault();
+		});
+		
+		window.addEventListener('keydown', function (e) {
+			if (!self.socket) {
+				return;
+			}
+			console.log('code ' + e.code);
+			self.socket.emit('key', e.code, true);
+		});
+		
+		window.addEventListener('keyup', function (e) {
+			if (!self.socket) {
+				return;
+			}
+			console.log('code ' + e.keyCode);
+			self.socket.emit('key', e.code, false);
+		});
 	}
 	
 	Mstsc.prototype = {
-		connect : function(ip, port) {
+		connect : function(ip, domain, username, password) {
 			var self = this;
-			var socket = io('http://localhost:3000');
-			socket.on('connect', function() {
+			this.socket = io('http://localhost:3000').on('connect', function() {
 				console.log('[mstsc.js] connected');
 			}).on('bitmap', function(bitmap) {
 				self.update(bitmap);
 			}).on('close', function() {
-				console.log('[mstsc.js] close')
-				socket.disconnect();
+				console.log('[mstsc.js] close');
+			}).on('error', function (err) {
+				console.log('[mstsc.js] error : ' + err.code + '(' + err.message + ')');
 			});
+			this.socket.emit('infos', {ip : ip, port : 3389, domain : domain, username : username, password : password});
 		},
 		
 		update : function(bitmap) {
-			console.log('receive bitmap isCompress : ' + bitmap.isCompress);
-			
 			var input = new Uint8Array(bitmap.data);
 			var inputPtr = Module._malloc(input.length);
 			var inputHeap = new Uint8Array(Module.HEAPU8.buffer, inputPtr, input.length);
@@ -56,7 +109,6 @@
 				['number', 'number', 'number', 'number', 'number', 'number'],
 				[outputHeap.byteOffset, bitmap.width, bitmap.height, inputHeap.byteOffset, input.length, 2]
 			);
-			console.log(res);
 			var output = new Uint8ClampedArray(outputHeap.buffer, outputHeap.byteOffset, ouputSize);
 			var imageData = this.ctx.createImageData(bitmap.width, bitmap.height);
 			imageData.data.set(output);
